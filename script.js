67affc271a69f30ff80345f89828b237800488f8
Shincy <shincyshnz@gmail.com>
2022-11-10 10:36:41 +0530

    on the mouseover event the squares background color is changed to black

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 
 
 function makeBoard(size = 2) {
-    //---if canvas already have squares then clear the canvas----
-    let child = canvasDiv.lastElementChild;
-    while (child) {
-        canvasDiv.removeChild(child);
-        child = canvasDiv.lastElementChild;
-    }
-    //-----create squares--eg: 16X16-----
+    /*---if canvas already have squares then clear the canvas----*/
+
+    let squares = document.querySelectorAll('.square-div');
+    squares.forEach(div => div.remove());
+
+    /*-----create squares--eg: 16X16-------------*/
+
     canvasDiv.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     canvasDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement('div');
-        // square.textContent = i + 1;
+        square.classList.add('square-div');
         square.style.border = "solid 1px lightgrey";
+        /* ---- color the square when mouse is placed over it */
+
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = "black";
+        })
+
         canvasDiv.insertAdjacentElement("beforeend", square);
     }
 }
